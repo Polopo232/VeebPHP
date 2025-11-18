5f2a60465a770f67dd1dfca1e01ab391a442c2e1
Opilane TTHK <opilane@tthk.ee>
2025-11-18 09:35:13 +0200

    git kasud on kirjutatid

--- gitkasutamine.php
+++ gitkasutamine.php
@@ -10,12 +10,14 @@ echo "
 </li>
 "
 ?>
-<li>Konfigureerumine</li>
+<li>
+    Konfigureerumine
 <pre>
     git config global user.name "Polopo232"
     git config user.gmail "nikiforovnikita08@gmail.com"
     git config --global --list
 </pre>
+</li>
 <li>
     Ssh võti loomine
     <pre>
@@ -32,4 +34,20 @@ echo "
     </pre>
 </li>
 <?php
-echo "<li>GITHUB projektiga sidumine</li>>";
+echo "<li>GITHUB projektiga sidumine
+    <pre>
+        git remote add origin git@github.com:Polopo232/VeebPHP.git
+        git branch -M main
+        git push -u origin main
+    </pre>
+</li>>
+<li>
+    Projekti kloonimine desktopi <br>
+    * Kontrolli et id rsa.pub võti on olemas .shh kaustas <br>
+    * GIT CMD on lahti ja <br>
+    <pre>
+        git clone git@github.com:Polopo232/VeebPHP.git
+    </pre>
+</li>
+</ol>
+";
