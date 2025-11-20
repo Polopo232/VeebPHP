2aa7211e38e454c06bb149422fa9428a7900321c
Opilane TTHK <opilane@tthk.ee>
2025-11-20 15:42:02 +0200

    tekstfunktsioonid

--- nav.php
+++ nav.php
@@ -4,6 +4,7 @@
         <li><a href="?link=gitkasutamine.php">Git käsud</a></li>
         <li><a href="?link=veebkal.php">Veebikalkalator</a></li>
         <li><a href="?link=ajafunktsiooni.php">Ajafunktsiooni</a></li>
+        <li><a href="?link=tekstfunktsioonid.php">Tekst Funktsioonid</a></li>
         <li><a href="https://nikitanikiforov24.thkit.ee/" target="_blank">Vana index</a></li>
     </ul>
 </nav>
