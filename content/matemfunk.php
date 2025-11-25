df5beae7493252adcf333dcb6e3e1b9eee73ab98
Opilane TTHK <opilane@tthk.ee>
2025-11-25 11:49:22 +0200

    div

--- content/matemfunk.php
+++ content/matemfunk.php
@@ -1,5 +1,8 @@
 <?php
 
+echo "<div class=blocks-container>";
+echo "<div class=blocks>";
+
 echo "<h2>Matemallised tehted</h2> <br>";
 
 $arv1 = 120;
@@ -10,6 +13,9 @@ echo "+ Liitmine = ".($arv1 + $arv2)."<br>";
 echo "- Lahutamine = ".($arv1 - $arv2)."<br>";
 echo "* Korrutise tulemus = ".$arv1*$arv2."<br>";
 echo "/ Jagatise tulemus = ".$arv1/$arv2."<br>";
+
+echo "</div>";
+echo "<div class=blocks>";
 
 echo "<h3>Matemaatilised funktsioonid</h3><br>";
 echo "Väiksem arv: ".min($arv1,$arv2)."<br>";
@@ -22,6 +28,9 @@ echo "Juhustlik arv: ".rand($arv1,$arv2)."<br>";
 echo "Astendamine: ".pow($arv1,$arv2)."<br>";
 echo "sqrt(arv) - Ruutjuur: ".sqrt($arv1)."<br>";
 echo "Pi: ".pi()."<br>";
+
+echo "</div>";
+echo "<div class=blocks>";
 
 echo "<h2>Omistamine operatorid</h2><br>";
 $x = 10;
@@ -47,4 +56,51 @@ echo $nimi."<br>";
 $format = "Tere %s %s, arv x: %d";
 printf($format,$perenimi,$nimi,$x);
 
-echo "<h2>Mõistatus. Arva ära Eesti linnanimi</h2><br>";
+echo "</div>";
+echo "<div class=blocks>";
+
+//5-6
+echo "<h2>Arv mõistatus. Arva ära arvu</h2><br>";
+
+$arv_1 = 67;
+$arv_2 = 5.2;
+
+echo "arv 1 + arv 2 = ".($arv_1 + $arv_2)."<br>";
+echo "arv 1 = round(sqrt(arv_1)) = ".round(sqrt($arv_1))."<br>";
+echo "round(arv 2) = ".round($arv_2)."<br>";
+echo "arv 1 * pi = ".($arv_1 * pi())."<br>";
+echo "sqrt((arv 1 + 8) + (arv 2 + 0.8)) = ".sqrt(($arv_1 + 8) + ($arv_2 + 0.8));
+
+echo "</div>";
+echo "</div>";  
+
+echo "<br> <br> <br>";
+?>
+
+    <form name="tekstkontroll" action="matemfunk.php" method="post">
+        <label for="arv1">Sisesta arv 1</label>
+        <input type="text" id="arv1" name="arv1">
+        <input type="submit" value="Kontroll">
+    </form>
+    <br>
+    <form name="tekstkontroll_2" action="matemfunk.php" method="post">
+        <label for="arv2">Sisesta arv 2</label>
+        <input type="text" id="arv2" name="arv2">
+        <input type="submit" value="Kontroll">
+    </form>
+
+<?php
+if(isset($_REQUEST["arv1"])){
+    if($_REQUEST["arv1"] == $arv_1){
+        echo $_REQUEST["arv1"]." on õige !";
+    } else {
+        echo $_REQUEST["arv1"]." on vale !";
+    }
+}
+if(isset($_REQUEST["arv2"])){
+    if($_REQUEST["arv2"] == $arv_2){
+        echo $_REQUEST["arv2"]." on õige !";
+    } else {
+        echo $_REQUEST["arv2"]." on vale !";
+    }
+}
