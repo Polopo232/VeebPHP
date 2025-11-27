d1984fe91184fec8460828eedd99c5f710aaef77
Opilane TTHK <opilane@tthk.ee>
2025-11-27 17:16:01 +0200

    muusika vorm

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
         <li><a href="?link=avaleht.php">Avaleht</a></li>
         <li><a href="?link=gitkasutamine.php">Git käsud</a></li>
         <li><a href="?link=veebkal.php">JS: Veebikalkalator</a></li>
-        <li><a href="?link=veebkal.php">JS: Veebikalkalator</a></li>
+        <li><a href="?link=musVorm.php">JS: Muusika Vorm</a></li>
         <li><a href="?link=ajafunktsiooni.php">Ajafunktsiooni</a></li>
         <li><a href="?link=tekstfunktsioonid.php">Tekst Funktsioonid</a></li>
         <li><a href="?link=matemfunk.php">Matematikaa Funktsioonid</a></li>
