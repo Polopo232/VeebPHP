fe3a9b61603248c2d7e6d3bf5fe437badfeae911
Nikita Nikiforov <130934938+Polopo232@users.noreply.github.com>
2025-12-09 20:29:55 +0200

    Add files via upload

---
+++ content/arvutipood/nav.php
@@ -0,0 +1,10 @@
+<header>
+    <h3><a href="tervislik_lehed.php">Arvutipood</a></h3>
+    <nav>
+        <ul>
+            <li><a href="tervislik_lehed.php">Meist</a></li>
+            <li><a href="tooted.php">Tooted</a></li>
+            <li><a href="index.php">Juhn</a></li>
+        </ul>
+    </nav>
+</header>
