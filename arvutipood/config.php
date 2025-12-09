c76cee580f19b0c6fa4626a8d781ce2eee0ab65c
Nikita Nikiforov <130934938+Polopo232@users.noreply.github.com>
2025-12-09 18:28:21 +0000

    Delete arvutipood directory

--- arvutipood/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//kasutame kohalik arvuti
-$servernimi = 'localhost';
-$kasutajanimi = 'arvutipood';
-$parool='12345';
-$andmebaasinimi = 'arvutipood';
-$connection = new mysqli($servernimi, $kasutajanimi, $parool, $andmebaasinimi);
-$connection->set_charset("utf8");
-
